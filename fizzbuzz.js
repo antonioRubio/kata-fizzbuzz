ed44e0cdd6b93a0dcab21f0c55c6ced946418e8b
gazapo23 <gazapo23@gmail.com>
2017-03-10 06:42:04 +0100

    refactor fizz and buzz to internal methods

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,42 +1,43 @@
-const MULTIPLE_OF_THREE = 3;
-const MULTIPLE_OF_FIVE = 5;
-
 module.exports = function FizzBuzz(database) {
+
+    const MULTIPLE_OF_THREE = 3;
+    const MULTIPLE_OF_FIVE = 5;
 
     this.database = database;
 
     this.print = function (number) {
-        checkIsInteger(number);
+        this.checkIsInteger(number);
         var result  = number;
-        if (isFizz(number) || isBuzz(number)) {
+        if (this.isFizz(number) || this.isBuzz(number)) {
             result = '';
             database.initConnection();
-            if (isFizz(number)) {
+            if (this.isFizz(number)) {
                 result +=  this.database.getStringWhenThreeNumber();
             }
-            if (isBuzz(number)) {
+            if (this.isBuzz(number)) {
                 result +=  this.database.getStringWhenFiveNumber();
             }
         }
 
         return result;
-    }
+    };
+
+    this.checkIsInteger = function (string) {
+        if (false === Number.isInteger(string)) {
+            throw new Error('Number is not an Integer');
+        }
+    };
+
+    this.isFizz = function (number) {
+        return this.isMultiple(number, MULTIPLE_OF_THREE);
+    };
+
+    this.isBuzz = function (number) {
+        return this.isMultiple(number, MULTIPLE_OF_FIVE);
+    };
+
+    this.isMultiple = function (number, multipleNumber) {
+        return 0 === number % multipleNumber;
+    };
 };
 
-function checkIsInteger(string) {
-    if (false === Number.isInteger(string)) {
-        throw new Error('Number is not an Integer');
-    }
-}
-
-function isFizz(number) {
-    return isMultiple(number, MULTIPLE_OF_THREE);
-}
-
-function isMultiple(number, multipleNumber) {
-    return 0 === number % multipleNumber;
-}
-
-function isBuzz(number) {
-    return isMultiple(number, MULTIPLE_OF_FIVE);
-}
